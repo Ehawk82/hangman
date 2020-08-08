11b4d10909f106714bdd0c6daab88d233cf410ba
Jason Graziano <mrehawk82@gmail.com>
2020-08-08 19:43:59 -0400

    initial setup

---
+++ docs/index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-16">
+            <title>A Hangman Game</title>
+            <meta name="description" content="guess the word before you run out of turns and ry not to hang the man">
+            <meta name="author" content="Jason Graziano">
+            <script src="src/js/libs.js"></script>
+            <script src="src/js/scripts.js"></script>
+            <link rel="stylesheet" href="src/css/styles.css">
+        </head>
+        <body id="body"></body>
+    </html>
+</html>
