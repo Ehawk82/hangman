8da3790cbc8b8771e72c3dbe2ae73e52577d6189
Jason Graziano <mrehawk82@gmail.com>
2020-08-10 12:51:07 -0400

    fixes #9

---
+++ docs/src/js/libs.js
@@ -0,0 +1,10 @@
+function createEle(x){ return document.createElement(x) };
+function makeFull(x) { return x.className = x.className + "_full" };
+function takeFull(x) { var y,z; return y = x.className.split('_full'), z = y[0], x.className = z };
+function deleteThis(x,y) { return setTimeout(function(){ x.remove()},y)};
+function saveLS(x, y) { return localStorage.setItem(x, JSON.stringify(y)) };
+function loadLS(x) { return localStorage.getItem(x) };
+function parseLS(x) { var y = loadLS(x), z = JSON.parse(y); return z };
+function clearLS() { return localStorage.clear() };
+function removeLSitem(x) { return localStorage.removeItem(x) };
+function LSinit(x,y) { var keyname = localStorage.getItem(x); if (!keyname || keyname === null) { localStorage.setItem(x, JSON.stringify(y));} };
