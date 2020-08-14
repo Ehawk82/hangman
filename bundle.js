2cec8f156e8c3ab874f3b3c7e4ea113750673c59
Jason Graziano <mrehawk82@gmail.com>
2020-08-13 21:15:28 -0400

    words added

--- bundle.js
+++ bundle.js
@@ -1,7 +1,7 @@
 var myBundle = (function (exports) {
 	'use strict';
 
-	const words = ['beacon', 'luminary', 'butt', 'violence'];
+	const words = ['beacon', 'luminary', 'butt', 'violence', 'begger', 'forfet', 'basic'];
 
 	exports.words = words;
 
